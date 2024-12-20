01a2200f31376f782d8dcee126909c68e4ee3217
Tejaswi H T <144477659+Tej13aswi@users.noreply.github.com>
2024-12-20 13:22:22 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,358 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header" style="background-color: black; color: white;">
+
+<div class="header">
+    <a href="home.php" class="logo">
+        <img src="images/logo1.png" width="80" height="60" alt="JobMate Logo">
+        <span class="jobmate-text">JobMate</span>
+    </a>
+</div>
+<font color="white">
+   <nav class="navbar" style="color: black;">
+      <a href="home.php" style="color: white;">home</a>
+      <a href="about.php" style="color: white;">about</a>
+      <a href="package.php" style="color: white;">Sectors</a>
+      <a href="book.php" style="color: white;">book</a>
+	<a href="login.php" style="color: white;">login</a></font>
+   </nav>
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home">
+
+   <div class="swiper home-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide" style="background:url(images/img20.JPG) no-repeat">
+            <div class="content">
+               <span style="color: black;">Find Trusted Local Workers Quickly and Easily</span>
+               <h3>JOBMATE</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/img-10.jpg) no-repeat">
+            <div class="content">
+               <span>Connecting You with Skilled Professionals for Every Job</span>
+               <h3>JOBMATE</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/home-slide-3.jpg) no-repeat">
+            <div class="content">
+               <span>Find Trusted help anytime, anywhere</span>
+               <h3>search what you want</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+         
+      </div>
+
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+
+   </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- services section starts  -->
+
+<section class="services">
+
+   <h1 class="heading-title"> our services </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/ic1.png" alt="">
+         <h3>Electrician</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/ic2.png" alt="">
+         <h3>Mechanic</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/ic3.png" alt="">
+         <h3>Carpenter</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/ic4.png" alt="">
+         <h3>Construction</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/ic5.png" alt="">
+         <h3>Home Maid</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/ic6.png" alt="">
+         <h3>welding</h3>
+      </div>
+
+   </div>
+
+
+<!-- services section ends -->
+
+<!-- home about section starts  -->
+</section>
+
+<section class="home-about">
+
+   <div class="image">
+      <img src="images/img12.jpeg" alt="">
+   </div>
+
+   <div class="content">
+      <h3>about us</h3>
+      <p>We are a platform dedicated to connecting customers with skilled workers for essential services like home repairs, cleaning, and construction. Our goal is to make it easy for people to find trustworthy, verified professionals while also empowering local workers by providing them with consistent job opportunities. Through our platform, we foster a stronger community where everyone benefits—customers receive quality services, and workers gain stable, meaningful employment.</p>
+      <a href="about.php" class="btn">read more</a>
+   </div>
+
+</section>
+
+<!-- home about section ends -->
+
+<!-- home packages section starts  -->
+
+<section class="home-packages">
+
+   <h1 class="heading-title"> our Sectors </h1>
+
+   <div class="box-container">
+      <div class="box">
+         <div class="image">
+            <img src="images/img9.jpeg" alt="">
+         </div>
+         <div class="content">
+            <h3>Pest Control & Extermination</h3>
+            <p>connects you with skilled pest control and extermination workers, offering services for everything.</p>
+            <a href="sector.php" class="btn">view</a>
+         </div>
+      </div>
+      
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img3.jpeg" alt="">
+         </div>
+         <div class="content">
+            <h3>Cleaning Services</h3>
+            <p>connects you with skilled cleaners, offering reliable services for everything clean and healthy.</p>
+            <a href="sector.php" class="btn">view</a>
+         </div>
+      </div>
+      <div class="box">
+         <div class="image">
+            <img src="images/img2.jpeg" alt="">
+         </div>
+         <div class="content">
+            <h3>Landscaping & Gardening</h3>
+            <p>connects you with skilled landscaping and gardening workers, offering reliable services for everything.</p>
+            <a href="sector.php" class="btn">view</a>
+         </div>
+      </div>
+
+     
+
+   </div><br><br>
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img10.jpeg" alt="">
+         </div>
+         <div class="content">
+            <h3>Event Services</h3>
+            <p>connects you with skilled event service workers, offering reliable services for everything.</p>
+            <a href="sector.php" class="btn">view</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img11.jpeg" alt="">
+         </div>
+         <div class="content">
+            <h3>Health & Wellness</h3>
+            <p>connects users with skilled health and wellness professionals, offering a wide range of services to support a healthier, balanced lifestyle.</p>
+            <a href="sector.php" class="btn">view</a>
+         </div>
+      </div>
+      
+      <div class="box">
+         <div class="image">
+            <img src="images/img8.jpeg" alt="">
+         </div>
+         <div class="content">
+            <h3>Automotive Services</h3>
+            <p>connects you with skilled automative services workers, offering reliable services for everything.</p>
+            <a href="sector.php" class="btn">view</a>
+         </div>
+      </div>
+
+   </div><br><br>
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img6.jfif" alt="">
+         </div>
+         <div class="content">
+            <h3>Home Repair & Maintainance</h3>
+            <p>connects you with skilled home repair and maintenance workers, offering reliable services for everything.</p>
+            <a href="sector.php" class="btn">view</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img7.jpeg" alt="">
+         </div>
+         <div class="content">
+            <h3>Daily Wage & Manual Labor</h3>
+            <p>connects you with skilled daily wage and manual labors, offering reliable services for everything.</p>
+            <a href="sector.php" class="btn">view</a>
+         </div>
+      </div>
+      
+      <div class="box">
+         <div class="image">
+            <img src="images/img4.jpeg" alt="">
+         </div>
+         <div class="content">
+            <h3>Construction & Renovation</h3>
+            <p>connects you with skilled construction and renovation workers, offering reliable services for everything.</p>
+            <a href="sector.php" class="btn">view</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"> <a href="package.php" class="btn">load more</a> </div>
+
+</section>
+
+<!-- home packages section ends -->
+
+<!-- home offer section starts  -->
+
+<section class="home-offer">
+   <div class="content">
+         </div>
+</section>
+
+<!-- home offer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +91-9380856059 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +91-9889054723</a>
+         <a href="#"> <i class="fas fa-envelope"></i> nidafathimaa2004@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> Hassan, India - 573201 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>jobmate</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ sector.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header"><img src="images/logo1.png" alt="Logo" >
+
+   <a href="home.php" class="logo">JobMate.</a>
+  
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/header-bg-1.png) no-repeat">
+   <h1>Sectors</h1>
+</div>
+
+
+<!-- packages section starts  -->
+
+<section class="home-packages">
+
+   <h1 class="heading-title"> our Sectors </h1>
+
+   <div class="box-container">
+      <div class="box">
+         <div class="image">
+            <img src="images/i1.jpeg" alt="">
+         </div>
+         <div class="content">
+            <h3>Electrician</h3>
+            <p>connects you with skilled electricians, offering reliable services for everything.</p>
+            <a href="profile.php" class="btn">view profiles</a>
+         </div>
+      </div>
+      
+
+      <div class="box">
+         <div class="image">
+            <img src="images/i3.jpeg" alt="">
+         </div>
+         <div class="content">
+            <h3>Painter</h3>
+            <p>connects you with skilled Painters, offering reliable services for everything clean.</p>
+            <a href="profile.php" class="btn">view profiles</a>
+         </div>
+      </div>
+      <div class="box">
+         <div class="image">
+            <img src="images/i2.jpeg" alt="">
+         </div>
+         <div class="content">
+            <h3>Plumber</h3>
+            <p>connects you with skilled Plumbers, offering reliable services for everything.</p>
+            <a href="profile.php" class="btn">view profiles</a>
+         </div>
+      </div>
+
+     
+
+   </div><br><br>
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/i4.jpeg" alt="">
+         </div>
+         <div class="content">
+            <h3>Carpenters</h3>
+            <p>connects you with skilled carpenters, offering reliable services for everything.</p>
+            <a href="profile.php" class="btn">view profiles</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/i5.jpeg" alt="">
+         </div>
+         <div class="content">
+            <h3>Handymen</h3>
+            <p>connects users with skilled Handymens, offering a wide range of services.</p>
+            <a href="profile.php" class="btn">view profiles</a>
+         </div>
+      </div>
+      
+      <div class="box">
+         <div class="image">
+            <img src="images/i6.jpeg" alt="">
+         </div>
+         <div class="content">
+            <h3>Appliance repair technicians</h3>
+            <p>connects you with skilled technicians, offering reliable services for everything.</p>
+            <a href="profile.php" class="btn">view profiles</a>
+         </div>
+      </div>
+
+   </div><br><br>
+  
+
+   
+
+   <div class="load-more"> <a href="package.php" class="btn">load more</a> </div>
+
+</section>
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> sector</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +91-9380856059 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +91-678967899 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> jobmatehelp@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> Hassan, India - 573201 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>jobmate</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
